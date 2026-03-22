b555e184a33b19eedd10eee9f21af02a5d4aedc8
Deepak Ram <154036627+Deepakram0929@users.noreply.github.com>
2026-03-22 15:59:40 +0530

    commit changes

---
+++ eks/backend.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.9.0, < 2.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.49.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "deepak-terraform-state-tfbackup"
+    region         = "us-east-1"
+    key            = "eks/terraform.tfstate"
+    dynamodb_table = "Lock-Files"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = var.aws-region
+}
